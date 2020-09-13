4fb92441aa6bf986a678a6fd50c414d2c3b9b82a
AndrewPru <58787232+AndrewPru@users.noreply.github.com>
2020-09-13 19:23:14 +0300

    grid add

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import '../App.css';
 import components from './components.module.css';
 import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
-import Main from "../Pages/Main";
+import Main from "../Pages/Main/Main";
 
 export default class Navigation extends Component {
     render() {
@@ -11,9 +11,9 @@ export default class Navigation extends Component {
                 <div id="main">
                     <nav className={components.wrap_nav}>
                         <Link className={components.wrap_nav__item} to="/">Главная</Link>
-                        <Link to="/price">Услуги</Link>
-                        <Link to="/news">Новости</Link>
-                        <Link to="/about">О нас</Link>
+                        <Link className={components.wrap_nav__item} to="/price">Услуги</Link>
+                        <Link className={components.wrap_nav__item} to="/news">Новости</Link>
+                        <Link className={components.wrap_nav__item} to="/about">О нас</Link>
                     </nav>
                     <Switch>
                         <Route path="/" component={Main}/>
