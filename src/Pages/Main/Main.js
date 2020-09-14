c4d4199034c11978b64511390ec230d93624c783
AndrewPru <58787232+AndrewPru@users.noreply.github.com>
2020-09-14 19:15:33 +0300

    Comments

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -7,22 +7,17 @@ import data from './data'
 import './Main.css'
 
 export default function Main() {
-    // Hook1: Tie media queries to the number of columns
     const columns = useMedia(['(min-width: 1500px)', '(min-width: 1000px)', '(min-width: 600px)'], [5, 4, 3], 2)
-    // Hook2: Measure the width of the container element
     const [bind, { width }] = useMeasure()
-    // Hook3: Hold items
     const [items, set] = useState(data)
-    // Hook4: shuffle data every 5 seconds
+
     useEffect(() => void setInterval(() => set(shuffle), 5000), [])
-    // Form a grid of stacked items using width & columns we got from hooks 1 & 2
-    let heights = new Array(columns).fill(0) // Each column gets a height starting with zero
+    let heights = new Array(columns).fill(0)
     let gridItems = items.map((child, i) => {
-        const column = heights.indexOf(Math.min(...heights)) // Basic masonry-grid placing, puts tile into the smallest column using Math.min
-        const xy = [(width / columns) * column, (heights[column] += child.height / 2) - child.height / 2] // X = container width / number of columns * column index, Y = it's just the height of the current column
+        const column = heights.indexOf(Math.min(...heights))
+        const xy = [(width / columns) * column, (heights[column] += child.height / 2) - child.height / 2]
         return { ...child, xy, width: width / columns, height: child.height / 2 }
     })
-    // Hook5: Turn the static grid values into animated transitions, any addition, removal or change will be animated
     const transitions = useTransition(gridItems, item => item.css, {
         from: ({ xy, width, height }) => ({ xy, width, height, opacity: 0 }),
         enter: ({ xy, width, height }) => ({ xy, width, height, opacity: 1 }),
@@ -31,7 +26,6 @@ export default function Main() {
         config: { mass: 5, tension: 500, friction: 500 },
         trail: 25
     })
-    // Render the grid
     return (
         <div {...bind} class="list" style={{ height: Math.max(...heights) }}>
             {transitions.map(({ item, props: { xy, ...rest }, key }) => (
